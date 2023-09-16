05363c2d4fcb0e826083167e3f9d1f9dca6358a6
vladimir9961 <vladimir.mil21@gmail.com>
2023-09-16 16:18:11 +0200

    fixing image

--- be-blog/routes/Blogs/postRoutes.js
+++ be-blog/routes/Blogs/postRoutes.js
@@ -29,7 +29,7 @@ router.post("/posts", verifyToken, upload.single("image"), async (req, res) => {
     // Koristite binarne podatke slike iz req.file.buffer
     const image = req.file ? req.file.buffer : null;
 
-    console.log("Binary image data:", image); // Dodajte ovu liniju za prikaz binarnih podataka slike
+    console.log("Binary image data:", image); // This line displays binary image data
 
     // Get the user's ID from the decoded token (assuming you're using JWT for authentication)
     const userId = req.user.userId;
