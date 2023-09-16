8faed7bdcb9c332d512bd7d632e31efa6395004d
vladimir9961 <vladimir.mil21@gmail.com>
2023-09-16 15:54:56 +0200

    fixing image

--- be-blog/routes/Blogs/postRoutes.js
+++ be-blog/routes/Blogs/postRoutes.js
@@ -1,33 +1,42 @@
 const express = require("express");
 const router = express.Router();
 const cors = require("cors");
+const fs = require("fs");
+const multer = require("multer");
 const PostModel = require("../../models/PostsModal");
 const GeolocationModel = require("../../models/GeolocationModel ");
 const verifyToken = require("../../middleware/authMiddleware");
 router.use(cors());
-router.use(express.json());
-const multer = require("multer");
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "uploads");
+    cb(null, "./public/images");
   },
   filename: function (req, file, cb) {
-    cb(null, file.originalname);
+    const timestamp = Date.now();
+    const filename = timestamp + "-" + file.originalname;
+    cb(null, filename);
   },
 });
-const upload = multer({ storage: storage });
+
+const upload = multer({
+  storage,
+  limits: { fileSize: 400000 },
+});
 // Create a POST endpoint to add data to posts.json
 router.post("/posts", verifyToken, upload.single("image"), async (req, res) => {
   try {
+    // Handle image file (req.file) here as needed
+    const imageUrl = req.file ? "/images/" + req.file.filename : null;
+
     // Get the user's ID from the decoded token (assuming you're using JWT for authentication)
     const userId = req.user.userId;
 
     // Create a new post document with the associated user ID
     const newPost = new PostModel({
-      title: req.body.title, // Dobijamo naslov iz tela zahteva
-      content: req.body.content, // Dobijamo sadržaj iz tela zahteva
-      image: req.body.image, // Dobijamo base64 sliku iz tela zahteva
+      title: req.body.title,
+      content: req.body.content,
+      imageUrl: imageUrl,
       userId: userId, // Associate the post with the logged-in user
     });
 
