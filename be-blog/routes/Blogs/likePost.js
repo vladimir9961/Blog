0d9875b6a60995e5c82e3f07d2c6961cbf17f6df
vladimir9961 <vladimir.mil21@gmail.com>
2023-09-17 00:58:14 +0200

    lajk

--- be-blog/routes/Blogs/likePost.js
+++ be-blog/routes/Blogs/likePost.js
@@ -3,38 +3,99 @@ const router = express.Router();
 const verifyToken = require("../../middleware/authMiddleware");
 const PostModel = require("../../models/PostsModal");
 
-router.post("/posts/:postId/like", verifyToken, async (req, res) => {
-  try {
+// PUT za lajkiranje, DELETE za dislajkiranje
+router
+  .route("/posts/:postId/like")
+  .put(verifyToken, async (req, res) => {
     const postId = req.params.postId;
+    const isLike = req.body.isLike; // True za lajkiranje, false za dislajkiranje
 
-    // Pronađite post sa datim postId
-    const post = await PostModel.findOne({ _id: postId });
+    try {
+      // Pronađite post sa datim postId
+      const post = await PostModel.findOne({ _id: postId });
 
-    if (!post) {
-      return res.status(404).json({ error: "Post not found" });
+      if (!post) {
+        return res.status(404).json({ error: "Post not found" });
+      }
+
+      // Proverite da li je korisnik već lajkovao ili dislajkovao post
+      const alreadyLiked = post.likes.some(
+        (like) => like.userId.toString() === req.user.userId
+      );
+
+      if (alreadyLiked) {
+        return res
+          .status(400)
+          .json({
+            error: "Korisnik je već lajkovao ili dislajkovao ovaj post.",
+          });
+      }
+
+      // Ako je isLike true, dodajte lajk, inače dodajte dislajk
+      if (isLike) {
+        post.likes.push({ userId: req.user.userId });
+      } else {
+        post.dislikes.push({ userId: req.user.userId });
+      }
+
+      await post.save();
+
+      // Vratite odgovor sa statusom 200 ili odgovarajućom porukom
+      res
+        .status(200)
+        .json({ message: "Post je lajkovan/dislajkovan.", success: true });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: "Došlo je do greške." });
     }
+  })
+  .delete(verifyToken, async (req, res) => {
+    const postId = req.params.postId;
+    const isLike = req.body.isLike; // True za lajkiranje, false za dislajkiranje
 
-    // Proverite da li je korisnik već lajkovao post
-    const alreadyLiked = post.likes.some(
-      (like) => like.userId.toString() === req.user.userId
-    );
+    try {
+      // Pronađite post sa datim postId
+      const post = await PostModel.findOne({ _id: postId });
 
-    if (alreadyLiked) {
-      return res
-        .status(400)
-        .json({ error: "Korisnik je već lajkovao ovaj post." });
+      if (!post) {
+        return res.status(404).json({ error: "Post not found" });
+      }
+
+      // Proverite da li je korisnik već lajkovao ili dislajkovao post
+      const likedIndex = post.likes.findIndex(
+        (like) => like.userId.toString() === req.user.userId
+      );
+      const dislikedIndex = post.dislikes.findIndex(
+        (dislike) => dislike.userId.toString() === req.user.userId
+      );
+
+      if (isLike && likedIndex === -1) {
+        return res
+          .status(400)
+          .json({ error: "Korisnik nije lajkovao ovaj post." });
+      } else if (!isLike && dislikedIndex === -1) {
+        return res
+          .status(400)
+          .json({ error: "Korisnik nije dislajkovao ovaj post." });
+      }
+
+      // Ako je isLike true, uklonite lajk, inače uklonite dislajk
+      if (isLike) {
+        post.likes.splice(likedIndex, 1);
+      } else {
+        post.dislikes.splice(dislikedIndex, 1);
+      }
+
+      await post.save();
+
+      // Vratite odgovor sa statusom 200 ili odgovarajućom porukom
+      res
+        .status(200)
+        .json({ message: "Lajk/dislajk je uklonjen.", success: true });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: "Došlo je do greške." });
     }
-
-    // Ažurirajte broj lajkova u Post modelu
-    post.likes.push({ userId: req.user.userId });
-    await post.save();
-
-    // Vratite odgovor sa statusom 200 ili odgovarajućom porukom
-    res.status(200).json({ message: "Post je lajkovan.", success: true });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: "Došlo je do greške." });
-  }
-});
+  });
 
 module.exports = router;
