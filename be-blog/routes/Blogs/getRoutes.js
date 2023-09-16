a2a3d81d34c7c7a3fe48f14f311ddf693104351b
vladimir9961 <vladimir.mil21@gmail.com>
2023-09-16 22:37:48 +0200

    username to get posts route

--- be-blog/routes/Blogs/getRoutes.js
+++ be-blog/routes/Blogs/getRoutes.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const PostModel = require("../../models/PostsModal");
-const UserModel = require("../../models/UserModel");
 // Create a GET endpoint to fetch posts data from MongoDB
 
 router.get("/posts", async (req, res) => {
@@ -9,28 +8,11 @@ router.get("/posts", async (req, res) => {
     // Fetch all posts from the MongoDB collection, uključujući i komentare
     const posts = await PostModel.find().populate("comments"); // Korišćenje populate za uključivanje komentara
 
-    // Iterirajte kroz postove i za svaki post pronađite korisnika po userId
-    const postsWithUsername = await Promise.all(
-      posts.map(async (post) => {
-        const user = await UserModel.findById(post.userId);
-        return {
-          title: post.title,
-          content: post.content,
-          imageUrl: post.imageUrl,
-          username: user.username, // Dodajte korisničko ime u rezultat
-          likes: post.likes,
-          comments: post.comments,
-          dislikes: post.dislikes,
-        };
-      })
-    );
-
-    // Return the posts with username as JSON response
-    res.json(postsWithUsername);
+    // Return the posts as JSON response
+    res.json(posts);
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: "An error occurred" });
   }
 });
-
 module.exports = router;
