482d3878a05bee43f87292459e921fc7cf174d87
vladimir9961 <vladimir.mil21@gmail.com>
2023-09-16 06:06:27 +0200

    cors fixes

--- be-blog/server.js
+++ be-blog/server.js
@@ -7,6 +7,7 @@ const cors = require("cors");
 app.use(express.json());
 const corsOptions = {
   origin: "http://localhost:4200", // Dodajte svoj lokalni URL
+  optionsSuccessStatus: 200,
 };
 
 app.use(cors(corsOptions));
