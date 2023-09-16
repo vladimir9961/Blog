9933446faf49f93aaedbbe49a3558c9bbcf5f9e0
vladimir9961 <vladimir.mil21@gmail.com>
2023-09-16 15:13:08 +0200

    fixing image

--- be-blog/routes/Blogs/postRoutes.js
+++ be-blog/routes/Blogs/postRoutes.js
@@ -1,39 +1,17 @@
 const express = require("express");
 const router = express.Router();
 const cors = require("cors");
-const fs = require("fs");
-const multer = require("multer");
 const PostModel = require("../../models/PostsModal");
 const GeolocationModel = require("../../models/GeolocationModel ");
 const verifyToken = require("../../middleware/authMiddleware");
 router.use(cors());
 router.use(express.json());
 
-const Storage = multer.diskStorage({
-  // destination: function (req, file, cb) {
-  // cb(null, "./public/images");
-  destination: "uploads",
-  filename: (req, file, cb) => {
-    cb(null, file.originalname);
-  },
-  // },
-  // filename: function (req, file, cb) {
-  //   const timestamp = Date.now();
-  //   const filename = timestamp + "-" + file.originalname;
-  //   cb(null, filename);
-  // },
-});
+const upload = require("multer")();
 
-const upload = multer({
-  storage: Storage,
-  limits: { fileSize: 400000 },
-}).single("testImage");
 // Create a POST endpoint to add data to posts.json
 router.post("/posts", verifyToken, upload.single("image"), async (req, res) => {
   try {
-    // Handle image file (req.file) here as needed
-    // const imageUrl = req.file ? "/images/" + req.file.filename : null;
-
     // Get the user's ID from the decoded token (assuming you're using JWT for authentication)
     const userId = req.user.userId;
 
@@ -41,7 +19,7 @@ router.post("/posts", verifyToken, upload.single("image"), async (req, res) => {
     const newPost = new PostModel({
       title: req.body.title, // Dobijamo naslov iz tela zahteva
       content: req.body.content, // Dobijamo sadržaj iz tela zahteva
-      imageUrl: { data: req.body.filename, contentType: "image/jpg" },
+      image: req.body.image, // Dobijamo base64 sliku iz tela zahteva
       userId: userId, // Associate the post with the logged-in user
     });
 
