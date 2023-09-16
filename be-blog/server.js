f64f0a00080b3a43b660b98129479f6b0f38f5c6
vladimir9961 <vladimir.mil21@gmail.com>
2023-09-16 06:14:30 +0200

    cors fixes

--- be-blog/server.js
+++ be-blog/server.js
@@ -10,7 +10,17 @@ const corsOptions = {
   optionsSuccessStatus: 200,
 };
 
-app.use(cors());
+app.use(function (req, res, next) {
+  //Enabling CORS
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type"
+  ),
+    "Accept, x-client-key, x-client-token, x-client-secret, Authorization";
+  next();
+});
 app.use("/public", express.static(path.join(__dirname)));
 app.use("/images", express.static(path.join(__dirname, "public/images")));
 
