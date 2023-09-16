72f9dafe15c45d66aa9a0273f292452f6cf363b9
vladimir9961 <vladimir.mil21@gmail.com>
2023-09-16 17:13:27 +0200

    fixing image

--- be-blog/models/PostsModal.js
+++ be-blog/models/PostsModal.js
@@ -2,10 +2,7 @@ const mongoose = require("mongoose");
 const postSchema = new mongoose.Schema({
   title: String,
   content: String,
-  imageUrl: {
-    data: Buffer, // Store binary image data
-    contentType: String, // Store the content type of the image (e.g., "image/jpeg")
-  },
+  imageUrl: String,
   userId: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "User",
