f5e5b2c8e986121ebd9aff3a577292eb85370ceb
vladimir9961 <vladimir.mil21@gmail.com>
2023-09-16 22:17:15 +0200

    username to get posts route

--- be-blog/routes/Blogs/getRoutes.js
+++ be-blog/routes/Blogs/getRoutes.js
@@ -6,10 +6,7 @@ const PostModel = require("../../models/PostsModal");
 router.get("/posts", async (req, res) => {
   try {
     // Fetch all posts from the MongoDB collection, uključujući i komentare
-    const posts = await PostModel.find().populate("comments").populate({
-      path: "user", // Pretpostavka: polje za korisnika u modelu Post se zove "user"
-      select: "username", // Odaberite samo polje "username" iz modela User
-    });
+    const posts = await PostModel.find().populate("comments"); // Korišćenje populate za uključivanje komentara
 
     // Return the posts as JSON response
     res.json(posts);
@@ -18,7 +15,6 @@ router.get("/posts", async (req, res) => {
     res.status(500).json({ error: "An error occurred" });
   }
 });
-
 // router.get("/geolocation", async (req, res) => {
 //   try {
 //     const geolocation = await GeolocationModel.find();
