89bef12480abe410f81d2567088cab96dfc0fdce
vladimir9961 <vladimir.mil21@gmail.com>
2023-09-17 20:16:01 +0200

    cors

--- be-blog/routes/Blogs/getComments.js
+++ be-blog/routes/Blogs/getComments.js
@@ -1,30 +1,23 @@
 const express = require("express");
 const router = express.Router();
 const PostModel = require("../../models/PostsModal");
-const UserModel = require("../../models/UserModel"); // Import your User model
 
 router.get("/posts/:postId/comments", async (req, res) => {
   try {
     const postId = req.params.postId;
 
     // Prikupite komentare za određeni post koristeći postId
-    const post = await PostModel.findById(postId).populate("comments");
+    const post = await PostModel.findById(postId).populate({
+      path: "comments",
+      populate: {
+        path: "userId",
+        select: "username", // Select the 'username' field
+      },
+    });
 
     if (!post) {
       return res.status(404).json({ error: "Post nije pronađen." });
     }
-
-    // Populate the 'comments' with user data ('username')
-    await post
-      .populate({
-        path: "comments",
-        populate: {
-          path: "userId",
-          model: UserModel, // Use your User model
-          select: "username", // Select only 'username' field
-        },
-      })
-      .execPopulate();
 
     const comments = post.comments;
 
