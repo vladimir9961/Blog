1c34db7a7acff20752c6382ff2030cfd432b2a2b
vladimir9961 <vladimir.mil21@gmail.com>
2023-09-17 18:08:28 +0200

    comments update to get username

--- be-blog/routes/Blogs/getRoutes.js
+++ be-blog/routes/Blogs/getRoutes.js
@@ -1,9 +1,3 @@
-const express = require("express");
-const router = express.Router();
-const PostModel = require("../../models/PostsModal");
-const UserModel = require("../../models/UserModel");
-// Create a GET endpoint to fetch posts data from MongoDB
-
 router.get("/posts", async (req, res) => {
   try {
     // Fetch all posts from the MongoDB collection, uključujući i komentare
@@ -12,13 +6,28 @@ router.get("/posts", async (req, res) => {
     const postsWithUsername = await Promise.all(
       posts.map(async (post) => {
         const user = await UserModel.findById(post.userId);
+
+        // Dohvatiti korisničko ime (username) korisnika koji je ostavio komentar
+        const commentsWithUsername = await Promise.all(
+          post.comments.map(async (comment) => {
+            const commentUser = await UserModel.findById(comment.userId);
+            return {
+              _id: comment._id,
+              text: comment.text,
+              userId: comment.userId,
+              username: commentUser.username, // Dodajte korisničko ime ovde
+              __v: comment.__v,
+            };
+          })
+        );
+
         return {
           title: post.title,
           content: post.content,
           imageUrl: post.imageUrl,
           username: user.username,
           likes: post.likes,
-          comments: post.comments,
+          comments: commentsWithUsername, // Postavite komentare sa korisničkim imenima
           dislikes: post.dislikes,
           userId: post.userId,
           post_id: post._id,
@@ -34,4 +43,3 @@ router.get("/posts", async (req, res) => {
     res.status(500).json({ error: "An error occurred" });
   }
 });
-module.exports = router;
